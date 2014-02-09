6c4b388e4e06225e6490a7eb74d4ea6ec232a39f
deivon.deivon@gmail.com <deivon.deivon@gmail.com>
2014-02-09 22:47:46 +0100

    Add pragma in MoveToFront.cpp.

--- algorithms/MoveToFront.cpp
+++ algorithms/MoveToFront.cpp
@@ -3,17 +3,26 @@
 #include "constants.hpp"
 using namespace std;
 
+
 void mtf_encode (char *buf_in, char *buf_out, int size) {
     char state[ALPHABET_SIZE];
- 
-    for(int i = 0; i <ALPHABET_SIZE; ++i)
-        state[i] = i;
+ 	#pragma omp parallel num_threads(8)
+    {
+    	#pragma omp for schedule(dynamic, 1) nowait firstprivate(ALPHABET_SIZE)
+    	for(int i = 0; i <ALPHABET_SIZE; ++i)
+        	state[i] = i;
+    }
         
     for (int i = 0; i<size; i++) {
         buf_out[i] = state[buf_in[i]-'A']+'A';
-        for (int j = 0; j<ALPHABET_SIZE; ++j)
-            if (state[j] < state[buf_in[i]-'A'])
-                ++state[j];
+        #pragma omp parallel num_threads(8)
+    	{
+    		#pragma omp for schedule(dynamic, 1) nowait firstprivate(ALPHABET_SIZE)
+       		for (int j = 0; j<ALPHABET_SIZE; ++j) {
+           		if (state[j] < state[buf_in[i]-'A'])
+                	++state[j];
+        	}
+        }
         state[buf_in[i]-'A'] = 0;
     }
 }
@@ -22,8 +31,12 @@ void mtf_decode (char *buf_in, char *buf_out, int size) {
     char state[ALPHABET_SIZE];
     char tmp;
  
-    for (int i = 0; i<ALPHABET_SIZE; ++i)
-        state[i] = i;
+    #pragma omp parallel num_threads(8)
+    {
+    	#pragma omp for schedule(dynamic, 1) nowait firstprivate(ALPHABET_SIZE)
+    	for(int i = 0; i <ALPHABET_SIZE; ++i)
+        	state[i] = i;
+    }
         
     for (int i = 0; i<size; ++i) {
         buf_out[i] = state[buf_in[i]-'A'];
