a05f8e0c51909b2853fe9170c08d6f276608580f
jakubbrzeski <jk.brzeski@gmail.com>
2014-02-23 19:09:46 +0100

    Added bwt in testAll

--- tests/testAll.cpp
+++ tests/testAll.cpp
@@ -10,47 +10,37 @@
 using namespace std;
 
 int main() {
-	int size;
-	scanf("%d", &size);
-	char * buf_1 = new char[size];
-	char * buf_2 = new char[size];
-	scanf("%s", buf_1);
-	
-	//T B-W buf_1 -> buf_2
-	for(int i = 0; i<size; i++)
-		buf_2[i] = buf_1[i];
-		
-		
-	//MoveToFront buf_2 -> buf_1
-	mtf_encode(buf_2, buf_1, size);
-	
-	
-	//huffman buf_1->result.txt
-	vector<char> result = huff_encode(buf_1, size, "result.txt");
-	
-	//end of coding
-	
-	
-	
-	//huffman result.txt -> buf_2
-	result = huff_decode("result.txt");
-	for(int i = 0; i<size; i++)
-		buf_2[i] = result[i];
-	
-	//MoveToFront buf_2 -> buf_1
-	mtf_decode(buf_2, buf_1, size);
-	
-	//T B-W buf_1 -> buf_2
-	for(int i = 0; i<size; i++)
-		buf_2[i] = buf_1[i];
-		
-	//end of decoding
-	
-	
-	for(int i = 0; i<size; i++)
-		printf("%c", buf_2[i]);
-	printf("\n");	
-	 
+    int size;
+    scanf("%d", &size);
+    char * buf_1 = new char[size];
+    char * buf_2 = new char[size];
+    char *in = new char[size], *out = new char[size];
+    scanf("%s", in);
+
+// encoding
+    //BWT
+    pair<char*,int> enc = seq_bwt_encode(in,size);
+    //MoveToFront 
+    mtf_encode(enc.first, buf_1, size);
+    //huffman buf_1->result.txt
+    vector<char> result = huff_encode(buf_1, size, "result.txt");
+
+// decoding	
+    //huffman result.txt -> buf_1
+    result = huff_decode("result.txt");
+    for(int i = 0; i<size; i++) buf_1[i] = result[i];
+    //MoveToFront buf_1 -> buf_2
+    mtf_decode(buf_1, buf_2, size);
+    //BWT -> out
+    out = seq_bwt_decode(make_pair(buf_2,enc.second),size);
+    
+    // check correctness
+    assert(strcmp(in,out)==0);
+    cout<<"OK"<<endl;
+
+    //printf("out:\n");
+    //printf("%s\n", out);
+
 	return 0;
 }
 
